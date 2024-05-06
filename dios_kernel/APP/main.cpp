8e854863e9ad4d7f766a7108606e5b27d7c63cf8
risettetwinings <frierenbeyondjourneysend@hotmail.com>
2024-05-06 22:53:06 +0800

    Build dios_kernel project

---
+++ dios_kernel/APP/main.cpp
@@ -0,0 +1,61 @@
+﻿/*-----------------------------------------------------------------------------------------
+**
+** Copyright: Juliana, 2024~
+**
+** Filename: main.cpp
+**
+** Description: This File defines the entry of the application
+**
+** Including Function:
+**		main
+**
+** Design Annotation:
+**
+** Author:
+**		Juliana, start writing this file in May 2024
+**
+** Modification History:
+**		May 6th 2024, create this file
+**		$Revision$
+**		$Date$
+**		$Author$
+**		$Log$
+**
+**-----------------------------------------------------------------------------------------
+*/
+/*-----------------------------------------------------------------------------------------
+**										  Include
+**-----------------------------------------------------------------------------------------
+*/
+
+
+/*-----------------------------------------------------------------------------------------
+**									Function Definition
+**-----------------------------------------------------------------------------------------
+*/
+/*.BH--------------------------------------------------------------------------------------
+**
+** Function Name: main
+**
+** Description: This function defines the entry of the application, create the controller and run
+**
+** Input Parameter:
+**      argc , number of application start parameters
+**      argv , parameter list
+**
+** Output Parameter:
+**
+** Return Value:
+**
+** Design Annotation:
+**
+** Modification History:
+**		May 6th 2024, create this function
+**
+**.EH--------------------------------------------------------------------------------------
+*/
+int main(int argc, char* argv[])
+{
+    return 0;
+}
+/* END of main */
